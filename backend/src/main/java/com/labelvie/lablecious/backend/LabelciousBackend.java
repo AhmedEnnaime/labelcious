2bff6cb241f7a7db753a9f321365655572f3002c
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-06-02 22:21:22 +0100

    feat: :sparkles: fixed the springboot dockerization and working on an issue in angular dockerization

--- backend/src/main/java/com/labelvie/lablecious/backend/LabelciousBackend.java
+++ backend/src/main/java/com/labelvie/lablecious/backend/LabelciousBackend.java
@@ -2,10 +2,17 @@ package com.labelvie.lablecious.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LabelciousBackend {
 
+	@RequestMapping("/")
+    public String home() {
+        return "Dockerizing Spring Boot Application";
+    } 
 	public static void main(String[] args) {
 		SpringApplication.run(LabelciousBackend.class, args);
 	}
