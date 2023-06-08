5301b9c09a5b3a5bb7ff0009d2123587e5946925
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-06-08 16:04:31 +0100

    feat: :sparkles: completed categories widget and starting in pates widget

--- labelcious_mobile/lib/pages/home.dart
+++ labelcious_mobile/lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mobilelabelcious/widgets/categoriesWidget.dart';
 import 'package:mobilelabelcious/widgets/homeAppBar.dart';
 
 class Home extends StatefulWidget {
@@ -31,36 +32,6 @@ class _HomeState extends State<Home> {
                   topLeft: Radius.circular(35), topRight: Radius.circular(35))),
           child: Column(children: [
             Container(
-              margin: EdgeInsets.symmetric(horizontal: 15),
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(left: 5),
-                    height: 50,
-                    width: 300,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Search here...",
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  Icon(
-                    Icons.camera_alt,
-                    color: Color(0xFF4C53A5),
-                    size: 25,
-                  )
-                ],
-              ),
-            ),
-            Container(
               alignment: Alignment.centerLeft,
               margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
               child: Text(
@@ -71,7 +42,20 @@ class _HomeState extends State<Home> {
                     color: Color(0xFF4C53A5)),
               ),
             ),
-            // Implement Categories widget
+            CategoriesWidget(),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              child: Text(
+                "Plates",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF4C53A5)),
+              ),
+            ),
+
+            // Implementation of PlatesWidget
           ]),
         ),
         Center(
---
+++ labelcious_mobile/lib/widgets/categoriesWidget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CategoriesWidget extends StatefulWidget {
+  const CategoriesWidget({super.key});
+
+  @override
+  State<CategoriesWidget> createState() => _CategoriesWidgetState();
+}
+
+class _CategoriesWidgetState extends State<CategoriesWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          for (int i = 0; i < 8; i++)
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 8),
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/pastry.png",
+                    width: 40,
+                    height: 40,
+                  ),
+                  Text(
+                    "Desserts",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Color(0xFF4C53A5)),
+                  )
+                ],
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
