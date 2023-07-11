a627c0dba45735fb44fefa567359d0844258f7c0
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2023-07-11 14:33:28 +0100

    refactoring : move navebare to shared/layouts/navebare

--- labelcious_web/src/app/app.module.ts
+++ labelcious_web/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './views/shared/layouts/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
